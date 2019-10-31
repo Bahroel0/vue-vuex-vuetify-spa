ed2096dc735b8f8d9e866901c531bceaab8aa4ca
Bahroel0 <bahrul123amaruddin@gmail.com>
2019-10-31 11:14:27 +0700

    update env and readme

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/project/iapt-iaps/" : "/",
+    process.env.NODE_ENV === "production"
+      ? process.env.VUE_APP_ROOT_DIRECTORY
+      : "/",
   transpileDependencies: ["vuetify"]
 };
